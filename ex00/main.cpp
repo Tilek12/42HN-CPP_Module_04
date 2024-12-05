58c40291a1881336e46f4c4c0351ab680de22b42
tkubanyc <tkubanyc@student.42heilbronn.de>
2024-12-05 17:20:31 +0100

    ex00: chore: Create main function

---
+++ ex00/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkubanyc <tkubanyc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 17:17:48 by tkubanyc          #+#    #+#             */
+/*   Updated: 2024/12/05 17:19:48 by tkubanyc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+int	main( void ) {
+
+	const Animal*	meta = new Animal();
+	const Animal*	j = new Dog();
+	const Animal*	i = new Cat();
+
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+
+
+	return 0;
+}
