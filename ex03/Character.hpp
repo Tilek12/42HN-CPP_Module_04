35e3628e9580e1a3016a8b4395b216c1d58c7337
tkubanyc <tkubanyc@student.42heilbronn.de>
2024-12-07 13:41:59 +0100

    ex03: chore: Create Character.hpp

---
+++ ex03/Character.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Character.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkubanyc <tkubanyc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/07 13:32:55 by tkubanyc          #+#    #+#             */
+/*   Updated: 2024/12/07 13:40:31 by tkubanyc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHARACTER_HPP
+# define CHARACTER_HPP
+
+# include "ICharacter.hpp"
+# include "AMateria.hpp"
+
+class	Character : public ICharacter {
+
+private:
+
+	std::string	_name;
+	AMateria*	_inventory[4];
+
+public:
+
+	Character( const std::string& name );
+	Character( const Character& other );
+	Character&	operator=( const Character& other );
+	~Character( void );
+
+	const std::string&	getName( void ) const override;
+	void				equip( AMateria* m ) override;
+	void				unequip( int idx ) override;
+	void				use( int idx, ICharacter& target ) override;
+
+};
+
+#endif
--- ex03/ICharacter.hpp
+++ ex03/ICharacter.hpp
@@ -6,7 +6,7 @@
 /*   By: tkubanyc <tkubanyc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 13:26:41 by tkubanyc          #+#    #+#             */
-/*   Updated: 2024/12/07 13:28:59 by tkubanyc         ###   ########.fr       */
+/*   Updated: 2024/12/07 13:35:53 by tkubanyc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ class	ICharacter {
 
 public:
 
-	virtual ~ICharacter() {}
-	virtual std::string const & getName() const = 0;
-	virtual void equip(AMateria* m) = 0;
-	virtual void unequip(int idx) = 0;
-	virtual void use(int idx, ICharacter& target) = 0;
+	virtual						~ICharacter( void ) {}
+	virtual std::string const&	getName( void ) const = 0;
+	virtual void				equip( AMateria* m ) = 0;
+	virtual void				unequip( int idx ) = 0;
+	virtual void				use( int idx, ICharacter& target ) = 0;
 
 };
 
