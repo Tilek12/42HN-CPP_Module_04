ed6acdb8f1f632d969dff82cd43720e3a50cd2fc
tkubanyc <tkubanyc@student.42heilbronn.de>
2024-12-07 11:34:22 +0100

    ex03: chore: Create Cure.hpp

---
+++ ex03/Cure.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cure.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkubanyc <tkubanyc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/07 11:29:18 by tkubanyc          #+#    #+#             */
+/*   Updated: 2024/12/07 11:33:55 by tkubanyc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CURE_HPP
+# define CURE_HPP
+
+# include "AMateria.hpp"
+
+class	Cure : public AMateria {
+
+public:
+
+	Cure( void );
+	Cure( const Cure& other );
+	Cure&	operator=( const Cure& other );
+	~Cure( void );
+
+	AMateria*	clone( void ) const override;
+	// void		use( ICharacter& target );
+
+};
+
+#endif
